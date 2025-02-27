2a20b4caf41b736d9487fa933a66538a6040e43b
Bảo <your-email@example.com>
2025-02-27 20:34:56 +0700

    first commit

---
+++ banh.js
@@ -0,0 +1,9 @@
+var swiper = new Swiper(".swiper-container", {
+    slidesPerView: 3,  
+    spaceBetween: 10,  
+    loop: true,        
+    autoplay: {
+        delay: 2000,   
+        disableOnInteraction: false,
+    },
+});
